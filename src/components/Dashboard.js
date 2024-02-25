6efc2083dd1fe864c8384e7db23955422dde80cd
Prabha <prabhavika.sree@gmail.com>
2024-02-25 22:56:22 +0530

    Pushed all files

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from './styles.module.css';
+import styles from '../styles.module.css';
 
 //Dashboard is displayed with add and list buttons
 const Dashboard = () => {
